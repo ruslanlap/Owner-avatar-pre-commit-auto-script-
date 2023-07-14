f4b200d3870cc0a4234e22fe9c0b339bf59eb53a
matvrus <matviivn@ucu.edu.ua>
2023-07-14 04:07:21 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Кольорові коди та емоджі
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -7,77 +9,11 @@ NC='\033[0m' # No Color
 CHECK_MARK="\xE2\x9C\x94"
 CROSS_MARK="\xE2\x9D\x8C"
 
-# Функція створення файлу .gitleaks.toml
-create_gitleaks_config() {
-    echo '[[rules]]
-regex = "API[_-]?KEY"
-tags = ["api-key", "token" ]' > .gitleaks.toml
-}
-
-# Визначення операційної системи
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    OS="linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    OS="darwin"
-else
-    echo -e "${RED}Операційна система не підтримується.${NC}"
+# Перевірка, чи є поточна директорія репозиторієм Git
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    echo -e "${RED}Поточна директорія не є репозиторієм Git.${NC}"
     exit 1
 fi
-
-echo -e "${BLUE}Операційна система: ${BOLD}$OS${NC}"
-
-# Визначення архітектури
-ARCH=$(uname -m)
-if [[ "$ARCH" == "x86_64" ]]; then
-    ARCH="amd64"
-elif [[ "$ARCH" == "aarch64" ]]; then
-    ARCH="arm64"
-else
-    echo -e "${RED}Архітектура не підтримується.${NC}"
-    exit 1
-fi
-
-echo -e "${BLUE}Архітектура: ${BOLD}$ARCH${NC}"
-
-# Завантаження та встановлення gitleaks
-echo -e "${GREEN}Завантаження та встановлення gitleaks...${NC}"
-
-RELEASE_URL=""
-if [[ "$OS" == "darwin" ]]; then
-    RELEASE_URL="https://github.com/gitleaks/gitleaks/releases/download/v8.16.4/gitleaks_8.16.4_darwin_x64.tar.gz"
-elif [[ "$OS" == "linux" ]]; then
-    if [[ "$ARCH" == "amd64" ]]; then
-        RELEASE_URL="https://github.com/gitleaks/gitleaks/releases/download/v8.16.4/gitleaks_8.16.4_linux_x64.tar.gz"
-    elif [[ "$ARCH" == "arm64" ]]; then
-        RELEASE_URL="https://github.com/gitleaks/gitleaks/releases/download/v8.16.4/gitleaks_8.16.4_linux_arm64.tar.gz"
-    fi
-fi
-
-if [[ -z "$RELEASE_URL" ]]; then
-    echo -e "${RED}Не вдалося знайти відповідну версію gitleaks для даної операційної системи та архітектури.${NC}"
-    exit 1
-fi
-
-# Завантаження та розархівування gitleaks
-curl -sSL "$RELEASE_URL" -o gitleaks.tar.gz
-tar -xf gitleaks.tar.gz
-chmod +x gitleaks
-
-# Копіювання gitleaks до /usr/local/bin/
-sudo cp gitleaks /usr/local/bin
-
-echo -e "${GREEN}gitleaks встановлено.${NC}"
-
-# Створення файлу .gitleaks.toml
-echo -e "${GREEN}Створення файлу .gitleaks.toml...${NC}"
-create_gitleaks_config
-
-# Видалення тимчасових файлів
-rm gitleaks.tar.gz gitleaks
-git restore README.md
-
-GITLEAKS_VERSION=$(gitleaks version)
-echo -e "Версія gitleaks: ${GREEN}${BOLD}${GITLEAKS_VERSION}!${NC}"
 
 # Визначте URL репозиторію, де знаходиться скрипт pre-commit
 REPO_URL="https://raw.githubusercontent.com/matvrus/pre-commit-auto-script/main/pre-commit.sh"
--- pre-commit.sh
+++ pre-commit.sh
@@ -7,7 +7,8 @@ BLUE='\033[0;34m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
-
+# Змінні конфігурації
+GIT_LEAKS_ENABLED_CONFIG="gitleaks.enabled"
 
 # Запуск gitleaks
 echo -e "${GREEN}Запуск gitleaks...${NC}"
@@ -22,3 +23,13 @@ if [[ $gitleaksExitCode -eq 1 ]]; then
 else
     echo -e "${GREEN}Перевірка секретів пройшла успішно.${NC}"
 fi
+
+# Вимкнути pre-commit hook
+echo -e "${BLUE}Вимкнення pre-commit hook...${NC}"
+git config $GIT_LEAKS_ENABLED_CONFIG false
+echo -e "${BLUE}Pre-commit hook вимкнено.${NC}"
+
+# Включити pre-commit hook
+echo -e "${BLUE}Включення pre-commit hook...${NC}"
+git config $GIT_LEAKS_ENABLED_CONFIG true
+echo -e "${BLUE}Pre-commit hook включено.${NC}"
