b25c1cd970efa11b64b8a4c0d253c0618fe70995
matvrus <matviivn@ucu.edu.ua>
2023-07-17 01:24:31 +0300

    fix

--- on-off-gitleaks.sh
+++ on-off-gitleaks.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # This function disables Gitleaks.
-function false() {
+function disable() {
   echo "Disable Gitleaks"
   git config core.hooksPath no-hooks
 }
 
 # Call the function to disable Gitleaks
-false
+disable
 
 # This function enable Gitleaks.
 function enable() {
   echo "Enable Gitleaks"
   git config --unset core.hooksPath
 }
-
-# Call the function to disable Gitleaks
-false
-
 # Call the function to enable Gitleaks
-false
+enable
