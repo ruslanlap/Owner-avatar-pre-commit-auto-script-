506b48262e5e03ee61f5c203a78e264b65a90cd8
matvrus <matviivn@ucu.edu.ua>
2023-07-17 02:31:03 +0300

    from ua to en

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-# Кольорові коди та емоджі
+#!/bin/bash
+
+# Color codes and emojis
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
@@ -11,41 +13,41 @@ create_gitleaks_config() {
     echo '[[rules]]
 regex = "API[_-]?KEY"
 tags = ["api-key", "token" ]' > .gitleaks.toml
-    
+
     # Download the additional configuration from the URL
     additional_config=$(curl -s https://raw.githubusercontent.com/gitleaks/gitleaks/master/config/gitleaks.toml)
-    
+
     # Append the additional configuration to the .gitleaks.toml file
     echo "$additional_config" >> .gitleaks.toml
 }
 
-# Визначення операційної системи
+# Determine the operating system
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     OS="linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     OS="darwin"
 else
-    echo -e "${RED}Операційна система не підтримується.${NC}"
+    echo -e "${RED}Unsupported operating system.${NC}"
     exit 1
 fi
 
-echo -e "${BLUE}Операційна система: ${BOLD}$OS${NC}"
+echo -e "${BLUE}Operating System: ${BOLD}$OS${NC}"
 
-# Визначення архітектури
+# Determine the architecture
 ARCH=$(uname -m)
 if [[ "$ARCH" == "x86_64" ]]; then
     ARCH="amd64"
 elif [[ "$ARCH" == "aarch64" ]]; then
     ARCH="arm64"
 else
-    echo -e "${RED}Архітектура не підтримується.${NC}"
+    echo -e "${RED}Unsupported architecture.${NC}"
     exit 1
 fi
 
-echo -e "${BLUE}Архітектура: ${BOLD}$ARCH${NC}"
+echo -e "${BLUE}Architecture: ${BOLD}$ARCH${NC}"
 
-# Завантаження та встановлення gitleaks
-echo -e "${GREEN}Завантаження та встановлення gitleaks...${NC}"
+# Download and install gitleaks
+echo -e "${GREEN}Downloading and installing gitleaks...${NC}"
 
 RELEASE_VERSION="v8.17.0" # Change the version number here
 RELEASE_URL=""
@@ -60,55 +62,56 @@ elif [[ "$OS" == "linux" ]]; then
 fi
 
 if [[ -z "$RELEASE_URL" ]]; then
-    echo -e "${RED}Не вдалося знайти відповідну версію gitleaks для даної операційної системи та архітектури.${NC}"
+    echo -e "${RED}Unable to find a compatible version of gitleaks for this operating system and architecture.${NC}"
     exit 1
 fi
 
-# Завантаження та розархівування gitleaks
+# Download and extract gitleaks
 curl -sSL "$RELEASE_URL" -o gitleaks.tar.gz
 tar -xf gitleaks.tar.gz
 chmod +x gitleaks
 
-# Копіювання gitleaks до /usr/local/bin/
-#sudo cp gitleaks /usr/local/bin
+# Copy gitleaks to /usr/local/bin/
+# sudo cp gitleaks /usr/local/bin
 
-echo -e "${GREEN}gitleaks встановлено.${NC}"
+echo -e "${GREEN}Gitleaks installed.${NC}"
 
-# Створення файлу .gitleaks.toml
-echo -e "${GREEN}Створення файлу .gitleaks.toml...${NC}"
+# Create .gitleaks.toml file
+echo -e "${GREEN}Creating .gitleaks.toml file...${NC}"
 create_gitleaks_config
 
-# Видалення тимчасових файлів
+# Remove temporary files
 rm gitleaks.tar.gz gitleaks
 git restore README.md
 
 GITLEAKS_VERSION=$(gitleaks version)
 echo -e "Gitleaks version: ${GREEN}${BOLD}${GITLEAKS_VERSION}!${NC}"
 
-# Визначте URL репозиторію, де знаходиться скрипт pre-commit
+# Define the URL of the repository where the pre-commit script is located
 REPO_URL="https://raw.githubusercontent.com/matvrus/pre-commit-auto-script/main/pre-commit.sh"
 
-# Визначте шлях до директорії hooks у вашому репозиторії Git
+# Define the path to the hooks directory in your Git repository
 HOOKS_DIR=".git/hooks"
 
-# Переконайтеся, що директорія hooks існує
+# Ensure that the hooks directory exists
 mkdir -p "$HOOKS_DIR"
 
-# Встановлення pre-commit hook
-echo -e "${BLUE}Завантаження pre-commit hook...${NC}"
+# Install the pre-commit hook
+echo -e "${BLUE}Downloading pre-commit hook...${NC}"
 curl -sSfL "$REPO_URL" -o "$HOOKS_DIR/pre-commit"
 chmod +x "$HOOKS_DIR/pre-commit"
 
 if [ -f "$HOOKS_DIR/pre-commit" ]; then
-    echo -e "${GREEN}${CHECK_MARK} Pre-commit hook script встановлено успішно!${NC}"
+    echo -e "${GREEN}${CHECK_MARK} Pre-commit hook script installed successfully!${NC}"
 else
-    echo -e "${RED}${CROSS_MARK} Виникла помилка під час встановлення pre-commit hook script.${NC}"
+    echo -e "${RED}${CROSS_MARK} An error occurred while installing the pre-commit hook script.${NC}"
 fi
+
 download-onoffscript() {
     # Download the gitleaks.sh file from the GitHub repository
     curl -sSfL "https://raw.githubusercontent.com/matvrus/pre-commit-auto-script/main/on-off-gitleaks.sh" -o on-off-gitleaks.sh
     chmod +x on-off-gitleaks.sh
-    }
-    
+}
+
 download-onoffscript
 rm LICENSE
--- pre-commit.sh
+++ pre-commit.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# Кольорові коди
+# Color codes for terminal output
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
+# Check if gitleaks is enabled in Git config
 ENABLE=$(git config --bool hooks.gitleaks-enable)
 
-# Запуск gitleaks
-echo -e "${GREEN}Запуск gitleaks...${NC}"
+# Running gitleaks to detect secrets in the repository
+echo -e "${GREEN}Running gitleaks...${NC}"
 gitleaksOutput=$(gitleaks detect --redact --verbose --report-format json --report-path gitleaks-report.json --config .gitleaks.toml)
 gitleaksExitCode=$?
 
+# Check if gitleaks found any secrets in the repository
 if [[ $gitleaksExitCode -eq 1 ]]; then
-    echo -e "${RED}Знайдено наступні секрети:${NC}"
+    echo -e "${RED}Found the following secrets:${NC}"
     echo "$gitleaksOutput"
-    echo -e "${RED}Заборонено затверджувати коміти з наявними секретами.${NC}"
+    echo -e "${RED}Committing with existing secrets is not allowed.${NC}"
     exit 1
 else
-    echo -e "${GREEN}Перевірка секретів пройшла успішно.${NC}"
+    echo -e "${GREEN}Secrets check passed successfully.${NC}"
 fi
 
+# Make the 'on-off-gitleaks.sh' script executable
 chmod +x on-off-gitleaks.sh
